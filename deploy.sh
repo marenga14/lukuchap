5c16287437e9a0e5c603d4918882c6754a48df78
marenga14 <marengajulius@gmail.com>
2022-08-23 13:29:54 +0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
-
-set e
-
+# abort on errors
+set -e
+# build
 yarn run build
-
+# navigate into the build output directory
 cd dist
-
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 git init
 git add -A
-
-git commit -m "another one"
-
-git push -f https://github.com/marenga14/lukuchap.git master:gh-pages 
-
+git commit -m 'deploy'
+git push -f git@github.com:marenga14/lukuchap.git master:gh-pages
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/my-project/'
+  ? '/lukuchap/'
   : '/',
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+			 //https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
   }
 })
